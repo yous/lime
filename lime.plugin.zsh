3d918f99383683ae1ef31a6b8358b7f2c6476d22
Chayoung You <yousbe@gmail.com>
2016-01-04 20:32:06 +0900

    Rewrite lime.plugin.zsh without oh-my-zsh

--- lime.plugin.zsh
+++ lime.plugin.zsh
@@ -1,8 +1,50 @@
-function prompt_char {
-	if [ $UID -eq 0 ]; then echo "#"; else echo $; fi
+prompt_lime_git() {
+  # Get VCS information
+  vcs_info
+
+  # Store working_tree without the 'x' prefix
+  local working_tree="${vcs_info_msg_1_#x}"
+  [[ -n $working_tree ]] || return
+
+  echo "%F{109}${vcs_info_msg_0_}$(prompt_lime_git_dirty)%f "
 }
 
-PROMPT='%{$fg[cyan]%}%n %{$fg[green]%}%~ $(git_prompt_info)%{$reset_color%}$(prompt_char) '
+prompt_lime_git_dirty() {
+  test -z "$(command git status --porcelain --ignore-submodules=dirty -unormal)"
+  (( $? )) && echo "*"
+}
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[cyan]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg[green]%} "
+prompt_lime_symbol() {
+  if [ $UID -eq 0 ]; then
+    echo '#'
+  else
+    echo '$'
+  fi
+}
+
+prompt_lime_setup() {
+  autoload -Uz vcs_info
+
+  zstyle ':vcs_info:*' enable git
+  # Export only two msg variables from vcs_info
+  zstyle ':vcs_info:*' max-exports 2
+  # %s: The current version control system, like 'git' or 'svn'
+  # %r: The name of the root directory of the repository
+  # #S: The current path relative to the repository root directory
+  # %b: Branch information, like 'master'
+  # %m: In case of Git, show information about stashes
+  # %u: Show unstaged changes in the repository (works with 'check-for-changes')
+  # %c: Show staged changes in the repository (works with 'check-for-changes')
+  #
+  # vcs_info_msg_0_ = '%b'
+  # vcs_info_msg_1_ = 'x%r' x-prefix prevents creation of a named path
+  #                         (AUTO_NAME_DIRS)
+  zstyle ':vcs_info:git*' formats '%b' 'x%r'
+  # '%a' is for action like 'rebase', 'rebase-i', 'merge'
+  zstyle ':vcs_info:git*' actionformats '%b(%a)' 'x%r'
+
+  setopt prompt_subst
+  PROMPT='%F{109}%n%f %F{143}%~%f $(prompt_lime_git)$(prompt_lime_symbol) '
+}
+
+prompt_lime_setup
