cd2428aa91934258cde209a0c5252c850a83af32
Chayoung You <yousbe@gmail.com>
2016-01-05 21:08:15 +0900

    Check git version for --ignore-submodules option
    Fix #1.

--- lime.plugin.zsh
+++ lime.plugin.zsh
@@ -1,3 +1,23 @@
+# Outputs -1, 0, or 1 if the installed git version is less than, equal to, or
+# greater than the input version, respectively
+git_compare_version() {
+  local input_version=(${(s/./)1})
+  local installed_version=($(command git --version 2>/dev/null))
+  installed_version=(${(s/./)installed_version[3]})
+
+  for i in {1..3}; do
+    if [[ $installed_version[$i] -gt $input_version[$i] ]]; then
+      echo 1
+      return 0
+    fi
+    if [[ $installed_version[$i] -lt $input_version[$i] ]]; then
+      echo -1
+      return 0
+    fi
+  done
+  echo 0
+}
+
 prompt_lime_user() {
   local prompt_color="${LIME_USER_COLOR:-109}"
   if (( ${LIME_SHOW_HOSTNAME:-0} )) && [[ -n "$SSH_CONNECTION" ]]; then
@@ -25,8 +45,13 @@ prompt_lime_git() {
 }
 
 prompt_lime_git_dirty() {
-  test -z "$(command git status --porcelain --ignore-submodules=dirty -unormal)"
-  (( $? )) && echo "*"
+  local git_status_options=(--porcelain -unormal)
+  if [[ "$prompt_lime_git_post_1_7_2" -ge 0 ]]; then
+    git_status_options+=(--ignore-submodules=dirty)
+  fi
+
+  test -z "$(command git status $git_status_options)"
+  (( $? )) && echo '*'
 }
 
 prompt_lime_symbol() {
@@ -58,8 +83,14 @@ prompt_lime_setup() {
   # '%a' is for action like 'rebase', 'rebase-i', 'merge'
   zstyle ':vcs_info:git*' actionformats '%b(%a)' 'x%r'
 
+  # The '--ignore-submodules' option is introduced on git 1.7.2
+  prompt_lime_git_post_1_7_2=$(git_compare_version '1.7.2')
+
   setopt prompt_subst
   PROMPT='$(prompt_lime_user) $(prompt_lime_dir) $(prompt_lime_git)$(prompt_lime_symbol) '
 }
 
 prompt_lime_setup
+
+# Clean up the namespace
+unfunction git_compare_version
